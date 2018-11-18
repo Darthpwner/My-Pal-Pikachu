40c299d767c4b3d46d78e9ba992e4eb8e84542bc
Matthew Allen Lin <matthewallenlin@matthews-macbook-pro-6.local>
2018-11-17 19:49:53 -0800

    Initial Commit

---
+++ My Pal PikachuTests/My_Pal_PikachuTests.swift
@@ -0,0 +1,36 @@
+//
+//  My_Pal_PikachuTests.swift
+//  My Pal PikachuTests
+//
+//  Created by Matthew Allen Lin on 11/17/18.
+//  Copyright © 2018 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import My_Pal_Pikachu
+
+class My_Pal_PikachuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
